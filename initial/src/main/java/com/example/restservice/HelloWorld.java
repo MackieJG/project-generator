64b32a4f4a4faac213a9eab87c8666e60a40908a
unknown <joshuamackie10@googlemail.com>
2024-04-22 10:58:35 +0100

    created function to randomly generate business and concept from two arrays

--- initial/src/main/java/com/example/restservice/HelloWorld.java
+++ initial/src/main/java/com/example/restservice/HelloWorld.java
@@ -1,7 +1,13 @@
 package com.example.restservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
 }
--- initial/src/main/java/com/example/restservice/RestServiceApplication.java
+++ initial/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -1,49 +1,32 @@
 package com.example.restservice;
 
+import java.util.Random;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
+@RestController
 public class RestServiceApplication {
+
+	private final String[] business = { "a coffee roastery", "a pizza shop", "a animal shelter",
+			"a street food vendor" };
+	private final String[] concepts = { "landing page", "a nav menu", "a chatbot", "an order review screen" };
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
+
 	}
 
-	@GetMapping("/business")
-	public Map<Integer, String> getBusiness() {
-		Map<Integer, String> response = new HashMap<>();
-		response.put(0, "a coffee roastery");
-		response.put(1, "a pizza shop");
-		response.put(2, "a animal shelter");
-		response.put(3, "a homeless shelter");
-		response.put(4, "a music store");
-		response.put(5, "a street food vendor");
-		response.put(6, "a community magazine");
-		response.put(7, "Iron Man");
-		response.put(8, "a brewery");
-		response.put(9, "a tour guide company");
-		return response;
-	}
+	@GetMapping("/")
+	public String getRandomItems() {
+		Random random = new Random();
 
-	@GetMapping("/concept")
-	public Map<Integer, String> getConcept() {
-		Map<Integer, String> response = new HashMap<>();
-		response.put(0, "landing page");
-		response.put(1, "discord bot");
-		response.put(2, "a set of button components");
-		response.put(3, "a nav menu");
-		response.put(4, "a scheduling app");
-		response.put(5, "an order review screen");
-		response.put(6, "a web UI kit");
-		response.put(7, "an automated tool");
-		response.put(8, "a chatbot");
-		response.put(9, "a translation tool");
-		return response;
+		return "{ \"business\": \"" + business[random.nextInt(business.length)] + "\", \"concept\": \""
+				+ concepts[random.nextInt(concepts.length)]
+				+ "\" }";
 	}
 
 }
